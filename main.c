be5459052517815d70717d678eb2937264f34d71
John Doe <daniel.mentzer@hotmail.com>
2023-03-02 13:08:50 +0100

    added picture for readme

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "header.h"
+// https://www.digikey.se/en/maker/projects/raspberry-pi-pico-and-rp2040-cc-part-1-blink-and-vs-code/7102fb8bca95452e9df6150f39ae8422
+
 // in build directory to build
 // cmake -G "MinGW Makefiles" ..
 // make
